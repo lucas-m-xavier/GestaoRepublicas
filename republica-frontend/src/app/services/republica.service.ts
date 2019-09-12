bdbae84b5a563b39c9a82ab2baecdc3412e719f8
Camatta <felipecamattabergi@gmail.com>
2019-09-12 18:01:06 -0300

    Spring 1 Completa

--- republica-frontend/src/app/services/republica.service.ts
+++ republica-frontend/src/app/services/republica.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Republica } from '../models/republica';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,45 @@ export class RepublicaService {
   constructor(private http: HttpClient) {
     this.republicasUrl = 'http://localhost:8080/republicas'
   }
+
+  form: FormGroup = new FormGroup({
+    $key: new FormControl(null),
+    nome: new FormControl('', Validators.required),
+    endereco: new FormControl('', Validators.required),
+    numeroVagas: new FormControl('', Validators.required),
+    tipoLocacao: new FormControl('', Validators.required),
+    genero: new FormControl('', Validators.required),
+    integrantes: new FormControl('', Validators.required),
+    numeroComodos: new FormControl('', Validators.required),
+    utensilios: new FormControl('', Validators.required),
+    diferencial: new FormControl('', Validators.required),
+    numeroVagasDisponiveis: new FormControl('', Validators.required),
+    descricao: new FormControl('', Validators.required),
+    representante: new FormControl('', Validators.required),
+    link: new FormControl('', Validators.required),
+    curso: new FormControl('', Validators.required)
+  });
+
+  /*
+  initializeFormGroup() {
+    this.form.setValue({
+      $key: null,
+      nome: '',
+      endereco: '',
+      numeroVagas: '',
+      tipoLocacao: '',
+      genero: '1',
+      integrantes: '',
+      numeroComodos: '',
+      utensilios: '',
+      diferencial: '',
+      numeroVagasDisponiveis: '',
+      descricao: '',
+      representante: '',
+      link: '',
+      curso: ''
+    });
+  }*/
 
   public findAll(): Observable<Republica[]> {
     return this.http.get<Republica[]>(this.republicasUrl)
